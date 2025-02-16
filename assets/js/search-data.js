b33388c1738192a77e244807f1fbdba15706a29a
Elfsong <dumingzhex@gmail.com>
2025-02-16 14:27:01 +0000

    Deploying to gh-pages from @ Elfsong/elfsong.github.io@c2e387ac2d82f47da923fe4734131ef78b0f65da 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
