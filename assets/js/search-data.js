7447395ef26d20ad3420db2f77be1ef68898ed5d
Elfsong <dumingzhex@gmail.com>
2025-11-24 08:39:58 +0000

    Deploying to gh-pages from @ Elfsong/elfsong.github.io@e2e20557da16df77bf30b795cce5a0d9c65e5613 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -132,17 +132,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/Codon/";
         
       },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
     },{id: "post-a-post-with-tabs",
       
         title: "a post with tabs",
@@ -372,17 +361,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2022/giscus-comments/";
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
     },{id: "post-a-post-with-redirect",
@@ -641,8 +619,8 @@ ninja.data = [{
           section: "News",},{id: "news-i-will-attend-emnlp-25-in-suzhou-china",
           title: 'I will attend EMNLP’25 in Suzhou, China.',
           description: "",
-          section: "News",},{id: "news-i-will-attend-neurips-25-in-san-diego-usa",
-          title: 'I will attend NeurIPS’25 in San Diego, USA.',
+          section: "News",},{id: "news-i-will-attend-neurips-25-in-mexico-city-mexico",
+          title: 'I will attend NeurIPS’25 in Mexico City, Mexico.',
           description: "",
           section: "News",},{id: "projects-ntu-x-nus",
           title: 'NTU x NUS',
