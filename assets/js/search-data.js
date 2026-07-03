7dafa1bdce3090a2c3977ccd40ff50350ec454fe
Elfsong <dumingzhex@gmail.com>
2026-07-03 11:57:33 +0000

    Deploying to gh-pages from @ Elfsong/elfsong.github.io@99128ef8dd5bc5e6e089c5b56cc13ca099b0a20c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4326,7 +4326,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%75%6D%69%6E%67%7A%68%65%78@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
